0afa50e8804a7dfc166a0a4bcdac0df0c9bc4e24
Stefan Eng <eng_steff@outlook.com>
2021-06-26 18:05:16 +0200

    Extend hello-world setup with static and template dirs.

--- sheet/app.py
+++ sheet/app.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "HELLO WORLD"
+    return render_template('index.html')
 
 def run():
     os.environ['FLASK_ENV'] = 'development'
