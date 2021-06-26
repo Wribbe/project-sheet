6e00a85ce0364e20293d163c26c7a0db225a35a1
Stefan Eng <eng_steff@outlook.com>
2021-06-26 17:43:06 +0200

    Add setup files for basic hello world.

---
+++ sheet.wsgi
@@ -0,0 +1,1 @@
+from sheet.app import app as application
---
+++ sheet/app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "HELLO WORLD"
+
+def run():
+    os.environ['FLASK_ENV'] = 'development'
+    app.run('0.0.0.0', debug=True)
